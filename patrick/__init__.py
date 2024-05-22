4d4f17aa44ec81a1e8d33e3ceb5b75f9b6c97a45
mikoonly <156190361+mikoonly@users.noreply.github.com>
2024-05-23 05:33:39 +0800

    Add files via upload

---
+++ patrick/__init__.py
@@ -0,0 +1,38 @@
+from patrick.core.bot import patrickbot
+from patrick.core.dir import dirr
+from patrick.core.git import git
+from patrick.core.userbot import Userbot
+from patrick.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = patrickbot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
