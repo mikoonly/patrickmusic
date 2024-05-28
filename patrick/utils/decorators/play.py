6563871bac4abf86ffd1516a84046180947603e5
mikoonly <156190361+mikoonly@users.noreply.github.com>
2024-05-28 18:16:00 +0800

    Update play.py

--- patrick/utils/decorators/play.py
+++ patrick/utils/decorators/play.py
@@ -1,6 +1,6 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from config import PLAYLIST_IMG_URL, PRIVATE_BOT_MODE, adminlist
+from config import PLAYLIST_IMG_URL, MUST_JOIN, PRIVATE_BOT_MODE, SUPPORT_GRUP, adminlist
 from strings import get_string
 from patrick import YouTube, app
 from patrick.misc import SUDOERS
@@ -126,3 +126,41 @@ def PlayWrapper(command):
         )
 
     return wrapper
+  
+def PlayWrapper(command):
+    async def wrapper(client, message):
+        language = await get_lang(message.chat.id)
+        _ = get_string(language)
+        if message.sender_chat:
+            upl = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="how to fix?,
+                            callback_data="AnonymousAdmin",
+                        ),
+                    ]
+                ]
+            )
+            return await message.reply_text(
+                _["general_3"], reply_markup=upl
+            )
+        if MUST_JOIN:
+            try:
+                await app.get_chat_member(MUST_JOIN, message.from_user.id)
+            except UserNotParticipant:
+                sub = await app.export_chat_invite_link(MUST_JOIN)
+                kontol = InlineKeyboardMarkup(
+                    [
+                        [InlineKeyboardButton("join dulu🗿", url=sub)]
+                    ]
+                )
+                return await message.reply_text(_["force_sub"].format(message.from_user.mention), reply_markup=kontol)
+                
+
+        if await is_maintenance() is False:
+            if message.from_user.id not in SUDOERS:
+                return await message.reply_text(
+                    text=f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ <a href={SUPPORT_GROUP}>Support Group</a> for knowing the reason.",
+                    disable_web_page_preview=True,
+                )
